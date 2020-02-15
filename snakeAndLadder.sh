aba7748111a747cfd9f661f6e6e385f7fcd73de3
Anjalik19 <Anjali.march19@gmail.com>
2020-02-15 12:39:48 +0530

    [Anjali]Add. added useCase4-WinningPosition

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,26 +1,51 @@
 #!/bin/sh
-a=10
-for(( i=$a; i>0; i--))
-     do
-	if(($(($i%2))==0))
-	     then
-		val=$((i*a))
-		gap=-1
+playerFunc()
+    {
+	echo "Snake and Ladder positions"
+	echo "START=1  FINISH=100"
+	echo "LADDERS : 6 to 27"
+	echo "        : 25 to 55"
+	echo "        : 37 to 65"
+	echo "        : 59 to 80"
+	echo "        : 74 to 93"
+	echo "SNAKES  : 39 to 3"
+	echo "        : 55 to 36"
+	echo "        : 61 to 2"
+	echo "        : 75 to 37"
+	echo "        : 98 to 70"
+
+	player=0
+
+ while((player<100))
+		do
+			#for dice
+			random=$(( 1 +  RANDOM % 6 ))
+			echo "Roll Dice = $random"
+
+			#for case
+			cases=$(( RANDOM % 3))
+			echo "case value : $cases"
+case $cases in
+0)
+	player=$player
+	echo "Player is at position : $player"
+;;
+1)
+	player=$(( player+random ))
+	echo "Position of player is : $player"
+
+;;
+2)
+	if(($((player-random))>0))
+	then
+	player=$((player-random))
+	echo "Player is at position : $player"
 	else
-		val=$(($(($i-1))*$a+1))
-		gap=1
+	player=$player
+	echo "Player is at position : $player"
 	fi
-for((j=$a; j>0; j--))
-    do
-	if(($i==1))
-	    then
-		echo -n "| $val  |"
-		val=$(($val+$gap))
-		     else
-			echo -n "| $val |"
-			val=$(($val+$gap))
-	fi
-    done
-echo
-echo "|----------------------------------------------------------|"
-  done
+;;
+esac
+done
+}
+playerFunc
