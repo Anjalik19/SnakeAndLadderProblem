e45f4d150f3e42b31305afa8c23ba8400439d0ba
Anjalik19 <Anjali.march19@gmail.com>
2020-02-15 17:54:24 +0530

    [Anjali]Merge. merged CompleteCode branch with useCase7-GameWithTwoPlayers branch

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-
 
 echo "         " "************************************"
 echo "         " "* WELCOME TO SNAKE AND LADDER GAME *"
 echo "         " "*                                  *"
 echo "         " "************************************"
-echo " "
+echo " " 
 a=10
 for(( i=$a; i>0; i--))
      do
@@ -18,64 +12,10 @@ for(( i=$a; i>0; i--))
 	     then
 		val=$((i*a))
 		gap=-1
-=======
-=======
->>>>>>> useCase5-ExactWinningPosition
-=======
->>>>>>> useCase6-CountOfDice
-playerFunc()
-    {
-	echo "Snake and Ladder positions"
-	echo "START=1  FINISH=100"
-	echo "LADDERS : 6 to 27"
-	echo "        : 25 to 55"
-	echo "        : 37 to 65"
-	echo "        : 59 to 80"
-	echo "        : 74 to 93"
-	echo "SNAKES  : 39 to 3"
-	echo "        : 55 to 36"
-	echo "        : 61 to 2"
-	echo "        : 75 to 37"
-	echo "        : 98 to 70"
-
-	player=0
-<<<<<<< HEAD
-
- while((player<100))
-		do
-			#for dice
-			random=$(( 1 +  RANDOM % 6 ))
-			echo "Roll Dice = $random"
-
-			#for case
-			cases=$(( RANDOM % 3))
-			echo "case value : $cases"
-<<<<<<< HEAD
-=======
-
->>>>>>> useCase5-ExactWinningPosition
-case $cases in
-0)
-	player=$player
-	echo "Player is at position : $player"
-;;
-1)
-<<<<<<< HEAD
-	player=$(( player+random ))
-	echo "Position of player is : $player"
-
-;;
-2)
-	if(($((player-random))>0))
-	then
-	player=$((player-random))
-	echo "Player is at position : $player"
->>>>>>> useCase4-WinningPosition
 	else
-	player=$player
-	echo "Player is at position : $player"
+		val=$(($(($i-1))*$a+1))
+		gap=1
 	fi
-<<<<<<< HEAD
 for((j=$a; j>0; j--))
     do
 	if(($i==1))
@@ -90,8 +30,12 @@ for((j=$a; j>0; j--))
 echo
 echo "|----------------------------------------------------------|"
   done
+
+
 =======
 echo "Game Started"
+
+
 =======
 rolls()
    {
@@ -102,6 +46,7 @@ rolls()
 			echo "Number after rolling Dice : $random"
    }
 rolls
+
 
 =======
 player()
@@ -139,8 +84,94 @@ esac
 }
 player
 
+
+======
+playerFunc()
+    {
+	echo "Snake and Ladder positions"
+	echo "START=1  FINISH=100"
+	echo "LADDERS : 6 to 27"
+	echo "        : 25 to 55"
+	echo "        : 37 to 65"
+	echo "        : 59 to 80"
+	echo "        : 74 to 93"
+	echo "SNAKES  : 39 to 3"
+	echo "        : 55 to 36"
+	echo "        : 61 to 2"
+	echo "        : 75 to 37"
+	echo "        : 98 to 70"
+
+	player=0
+
+ while((player<100))
+		do
+			#for dice
+			random=$(( 1 +  RANDOM % 6 ))
+			echo "Roll Dice = $random"
+
+			#for case
+			cases=$(( RANDOM % 3))
+			echo "case value : $cases"
+case $cases in
+0)
+	player=$player
+	echo "Player is at position : $player"
+;;
+1)
+	player=$(( player+random ))
+	echo "Position of player is : $player"
+
+;;
+2)
+	if(($((player-random))>0))
+	then
+	player=$((player-random))
+	echo "Player is at position : $player"
+	else
+	player=$player
+	echo "Player is at position : $player"
+	fi
+;;
+esac
+done
+}
+playerFunc
+
+
 =======
-=======
+playerFunc()
+    {
+	echo "Snake and Ladder positions"
+	echo "START=1  FINISH=100"
+	echo "LADDERS : 6 to 27"
+	echo "        : 25 to 55"
+	echo "        : 37 to 65"
+	echo "        : 59 to 80"
+	echo "        : 74 to 93"
+	echo "SNAKES  : 39 to 3"
+	echo "        : 55 to 36"
+	echo "        : 61 to 2"
+	echo "        : 75 to 37"
+	echo "        : 98 to 70"
+
+	player=0
+
+ while((player<100))
+		do
+			#for dice
+			random=$(( 1 +  RANDOM % 6 ))
+			echo "Roll Dice = $random"
+
+			#for case
+			cases=$(( RANDOM % 3))
+			echo "case value : $cases"
+
+case $cases in
+0)
+	player=$player
+	echo "Player is at position : $player"
+;;
+1)
 	if(($((player+random))>100))
 	then
 	player=$player
@@ -159,17 +190,30 @@ player
 	player=$player
 	echo "Player is at position : $player"
 	fi
->>>>>>> useCase5-ExactWinningPosition
 ;;
 esac
 done
 }
 playerFunc
-<<<<<<< HEAD
->>>>>>> useCase4-WinningPosition
+
+
 =======
->>>>>>> useCase5-ExactWinningPosition
-=======
+playerFunc()
+    {
+	echo "Snake and Ladder positions"
+	echo "START=1  FINISH=100"
+	echo "LADDERS : 6 to 27"
+	echo "        : 25 to 55"
+	echo "        : 37 to 65"
+	echo "        : 59 to 80"
+	echo "        : 74 to 93"
+	echo "SNAKES  : 39 to 3"
+	echo "        : 55 to 36"
+	echo "        : 61 to 2"
+	echo "        : 75 to 37"
+	echo "        : 98 to 70"
+
+	player=0
 	count=0
 
 while((player<100))
@@ -214,6 +258,12 @@ case $cases in
 	fi
 ;;
 esac
+	done
+
+}
+playerFunc
+
+
 =======
 diceRoll()
 {
@@ -324,12 +374,7 @@ count=$((count+1))
 			fi
 	fi
 
->>>>>>> useCase7-GameWithTwoPlayers
 	done
 
 }
 playerFunc
-<<<<<<< HEAD
->>>>>>> useCase6-CountOfDice
-=======
->>>>>>> useCase7-GameWithTwoPlayers
